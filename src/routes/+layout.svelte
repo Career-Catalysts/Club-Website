6d7f73be0ab8d467571aa1a912cb73f3d15701d4
Surendra Jammishetti <suri312006@gmail.com>
2023-11-04 12:42:04 -0700

    finished basic animation
    Co-authored-by: kenricqq <kenricqq@users.noreply.github.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-	import '../app.postcss'
-	import { ModeWatcher } from 'mode-watcher'
-	import Header from './Header.svelte'
+  import "../app.postcss";
+  import { ModeWatcher } from "mode-watcher";
+  import Header from "./Header.svelte";
 </script>
 
 <ModeWatcher />
 
 <Header />
 
-<main class='flex justify-center items-center text-l'>
-	<slot />
+<main>
+  <slot />
 
-	<!-- <Footer /> -->
+  <!-- <Footer /> -->
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,125 +1,153 @@
 <script lang="ts">
-	import Button from '$lib/components/ui/button/button.svelte'
+  import Button from "$lib/components/ui/button/button.svelte";
+  let out_col = "grey";
+  let team_col = "blue";
+//   const pauseAnimation = () => {
+//     const team = document.getElementsByClassName(
+//       "team"
+//     ) as HTMLCollectionOf<HTMLDivElement>;
+//     for (const t of team) {
+//       if (t.style) {
+//         t.style.animationPlayState = "paused";
+//       } else {
+//         console.log("team is null");
+//       }
+//     }
+//   };
 
-	const pauseAnimation = () => {
-		const team = document.getElementsByClassName(
-			'team'
-		) as HTMLCollectionOf<HTMLDivElement>
-		for (const t of team) {
-			if (t.style) {
-				t.style.animationPlayState = 'paused'
-			} else {
-				console.log('team is null')
-			}
-		}
-	}
+//   const resumeAnimation = () => {
+//     const team = document.getElementsByClassName(
+//       "team"
+//     ) as HTMLCollectionOf<HTMLDivElement>;
+//     for (const t of team) {
+//       if (t.style) {
+//         t.style.animationPlayState = "running";
+//       } else {
+//         console.log("wtf happened");
+//       }
+//     }
+//   };
 </script>
 
 <!-- <div class="max-w-6xl"> -->
-<div class="flex flex-1 min-w-[50rem] w-full max-w-[68rem] mx-5 rounded-md p-5">
-	<section class="w-[50%]">
-		<h1 class="mb-3 max-w-2xl text-7xl font-medium">
-			Turning Interviews Into Opportunities.
-		</h1>
-		<h2 class="max-w-2xl text-5xl font-semibold">Together We Succeed.</h2>
-	</section>
+<div>
+  <div class="flex align-center justify-evenly">
+    <aside class="flex justify-center w-[60%]">
+      <section>
+        <h1 class="mb-11 max-w-2xl text-8xl font-medium">
+          Turning Interviews Into Opportunities.
+        </h1>
+        <h2 class="max-w-2xl text-6xl font-semibold">Together We Succeed.</h2>
+      </section>
+    </aside>
 
-	<section class="w-[50%]">
-		<Button on:click={pauseAnimation}>Pause</Button>
-		<svg class=" border-emerald-500 border-4" viewBox="0 0 500 500">
-			<!-- <path
-				pathLength="1"
-				stroke="black"
-				stroke-width="2"
-				fill="none"
-				d="M 200 100 L 300 100 V 350 H 450 M 300 350 L 450 350 V 100 H 300"
-				/>
-				<path
-				pathLength="1"
-				stroke="black"
-				stroke-width="2"
-				fill="none"
-				d="M 200 200 L 300 200 V 350 H 300"
-				/>
-				<path
-				pathLength="1"
-				stroke="black"
-				stroke-width="2"
-				fill="none"
-				d="M 200 300 L 300 300 V 350 H 300"
-				/> -->
-			<path
-				id="outliers"
-				pathLength="1"
-				stroke="black"
-				stroke-width="3"
-				fill="none"
-				d="M 100 100 l 70 5 l 30 20 l 50 -30 l 50 40 l 50 0 l 30 -20 l 30 10 20 0"
-			/>
-			<path
-				class="team"
-				pathLength="1"
-				stroke="purple"
-				stroke-width="3"
-				fill="none"
-				d="M 100 200 l 100 0 l 50 15 l 50 0 l 70 -10 l 70 50 l 40 0"
-			/>
-			<path
-				class="team"
-				pathLength="1"
-				stroke="purple"
-				stroke-width="3"
-				fill="none"
-				d="M 100 250 l 100 0 l 50 -5 l 30 0 l 50 30 l 50 0 l 50 -10 l 50 0"
-			/>
-			<path
-				class="team"
-				pathLength="1"
-				stroke="purple"
-				stroke-width="3"
-				fill="none"
-				d="M 100 300 l 100 0 l 30 -10 l 50 30 l 70 -20 l 50 0 l 50 -25 l 30 0"
-			/>
-		</svg>
-		<!-- <img src="github.svg" alt="" width="100" height="100" /> -->
-	</section>
+    <aside class="flex justify-center align-center w-[40%]">
+      <section class="w-full">
+        <!-- <Button on:click={pauseAnimation}>Pause</Button>
+        <Button on:click={resumeAnimation}>Resume</Button> -->
+        <svg class=" border-emerald-500 border-4" viewBox="0 0 500 500">
+          <path
+            class="outliers"
+            pathLength="1"
+            stroke={out_col}
+            stroke-width="3"
+            fill="none"
+            d="M 100 50 l 0 50 l -25 50 l 0 50 l 25 25 l 0 25 l -30 30 l 0 50"
+          />
+          <path
+            class="team"
+            pathLength="1"
+            stroke={team_col}
+            stroke-width="3"
+            fill="none"
+            d="M 175 50 l 0 50 l 35 50 l 0 150 l -25 50"
+          />
+          <path
+            class="team"
+            pathLength="1"
+            stroke={team_col}
+            stroke-width="3"
+            fill="none"
+            d="M 250 50 l 0 50 l 0 50 l 0 150 l 0 50"
+          />
+          <path
+            class="team"
+            pathLength="1"
+            stroke={team_col}
+            stroke-width="3"
+            fill="none"
+            d="M 325 50 l 0 50 l -35 50 l 0 150 l 25 50"
+          />
+
+          <path
+            class="outliers"
+            pathLength="1"
+            stroke={out_col}
+            stroke-width="3"
+            fill="none"
+            d="M 400 50 l 0 50 l 25 50 l 0 25 l -50 50 l 0 25 l 50 40 l 0 50 "
+          />
+
+
+
+		  <path d="M143 370c0-11.0457 8.95431-20 20-20h173c11.046 0 20 8.95431 20 20v45c0 11.0457-8.9547 20-20 20h-173c-11.0457 0-20-8.9543-20-20v-200z" fill="#90EE90"/>
+
+
+
+
+
+
+
+
+
+
+        </svg>
+        <!-- <img src="github.svg" alt="" width="100" height="100" /> -->
+      </section>
+    </aside>
+  </div>
 </div>
 
 <style>
-	path {
-		stroke-dasharray: 1;
-		/* sets dash length */
+  path {
+    stroke-dasharray: 1;
+    /* sets dash length */
 
-		stroke-dashoffset: 0;
-		stroke-linecap: round;
-		stroke-linejoin: round;
-		/* Draw once and done. */
-		/* animation: dash 10s linear forwards; */
+    stroke-dashoffset: 0;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    /* Draw once and done. */
+    /* animation: dash 10s linear forwards; */
 
-		/* Keep drawing */
-		animation: dash 5s ease-in-out infinite alternate;
-		/* animation-delay: 2000ms; */
-		/* animation-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1.275); */
-	}
+    /* Keep drawing */
+    animation: dash 5s ease-in-out infinite alternate;
 
-	#outliers {
-		stroke-dasharray: 1;
-		stroke-dashoffset: 0;
-		stroke-linecap: round;
-		stroke-linejoin: round;
-		/* Draw once and done. */
-		/* animation: dash 5s linear forwards; */
+    /* animation-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1.275); */
+  }
 
-		/* Keep drawing */
-		animation: dash 6s ease-in-out infinite alternate;
-	}
+  @keyframes dash {
+    /* from {
+      stroke-dashoffset: 1;
+    }
+    to {
+      stroke-dashoffset: 0;
+    } */
 
-	@keyframes dash {
-		from {
-			stroke-dashoffset: 1;
-		}
-		to {
-			stroke-dashoffset: 0;
-		}
-	}
+	0% {
+      stroke-dashoffset: 1;
+    }
+    70% {
+      stroke-dashoffset: 0;
+    }
+    100% {
+      stroke-dashoffset: 0;
+    }
+  }
+
+  @keyframes pause {
+    0%,
+    100% {
+      stroke-dashoffset: 0;
+    }
+  }
 </style>
