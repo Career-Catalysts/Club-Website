51a20e77741500f473f1163513f111a2eafb3e01
Surendra Jammishetti <suri312006@gmail.com>
2023-11-04 10:52:07 -0700

    looks better

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../app.postcss'
+	import { ModeWatcher } from 'mode-watcher'
+	import Header from './Header.svelte'
+</script>
+
+<ModeWatcher />
+
+<Header />
+
+<main class='flex justify-center items-center text-l'>
+	<slot />
+
+	<!-- <Footer /> -->
+</main>
