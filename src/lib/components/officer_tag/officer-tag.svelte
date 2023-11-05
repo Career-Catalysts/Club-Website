e6917e90974eb2d938b9242136b4a89d38641faa
Surendra Jammishetti <suri312006@gmail.com>
2023-11-04 22:44:26 -0700

    working on about us page

---
+++ src/lib/components/officer_tag/officer-tag.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import * as Avatar from "$lib/components/ui/avatar";
+</script>
+
+<div class="w-[45rem] h-[22rem] rounded-md mx-5 flex items-start px-10 py-8 bg-gradient-to-t from-cyan-600 to-blue-700">
+    <div class="flex flex-col items-center">
+      <Avatar.Root class="flex-shrink-0">
+        <Avatar.Image src="officer_pics/Default_pfp.svg" alt="officerpic" />
+        <Avatar.Fallback>CN</Avatar.Fallback>
+      </Avatar.Root>
+      <h1 class="text-2xl  font-semibold mt-2">Name</h1>
+    </div>
+  </div>
+  
+  
+<style>
+</style>
--- src/lib/components/ui/avatar/avatar.svelte
+++ src/lib/components/ui/avatar/avatar.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
 	import { Avatar as AvatarPrimitive } from "bits-ui";
 	import { cn } from "$lib/utils";
-
+  
 	type $$Props = AvatarPrimitive.Props;
-
+  
 	let className: $$Props["class"] = undefined;
 	export let delayMs: $$Props["delayMs"] = undefined;
+	export let height: string = "h-60"; // Default height
+	export let width: string = "h-60";   // Default width
 	export { className as class };
-</script>
-
-<AvatarPrimitive.Root
+  </script>
+  
+  <AvatarPrimitive.Root
 	{delayMs}
+	
 	class={cn(
-		"relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
-		className
+	  "relative flex shrink-0 overflow-hidden rounded-full",
+	  height, // Include the 'height' prop
+	  width,  // Include the 'width' prop
+	  className
 	)}
 	{...$$restProps}
->
+  >
 	<slot />
-</AvatarPrimitive.Root>
+  </AvatarPrimitive.Root>
