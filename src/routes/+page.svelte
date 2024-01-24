264f57233402b25e3f73bebe3ceab589acb528bd
Kenric Tee <kenrictee@Kenrics-MacBook-Air.local>
2024-01-24 11:35:32 -0800

    fixed base commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <script lang='ts'>
+
+</script>
+
+ -->
+<script>
+	import { DeviceMockup } from 'flowbite-svelte'
+
+	import { Button } from '$lib/components/ui/button'
+</script>
+
+<DeviceMockup device="ios">
+	<div class="p-20">
+		<h1>Welcome to SvelteKit</h1>
+		<p>
+			Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+		</p>
+	</div>
+
+	<Button>Button</Button>
+</DeviceMockup>
+
+<DeviceMockup device="laptop">
+	<div class="p-20">
+		<h1>Welcome to SvelteKit</h1>
+		<p>
+			Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+		</p>
+	</div>
+</DeviceMockup>
